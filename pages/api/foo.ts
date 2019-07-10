9310f55897b3d2ffe7b0b58250689cec3992a858
naoki.tomita <mxcpy608@hotmail.com>
2019-07-10 09:26:53 +0900

    Fix lint.

--- pages/api/foo.ts
+++ pages/api/foo.ts
@@ -2,4 +2,4 @@ import { NextApiResponse, NextApiRequest } from "next";
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
   res.end(JSON.stringify({ foo: "bar", body: req.body }));
-}
+};
--- pages/qiita/index.tsx
+++ pages/qiita/index.tsx
@@ -5,8 +5,8 @@ import fetch from "node-fetch";
 interface QiitaItem {
   rendered_body: string;
   body: string;
-  id: string,
-  likes_count: number,
+  id: string;
+  likes_count: number;
   tags: Array<{ name: string }>;
   title: string;
   url: string;
@@ -16,55 +16,66 @@ interface Props {
   items: QiitaItem[];
 }
 
-const Qiita: NextPage<Props, Props> = (props) => {
+const Qiita: NextPage<Props, Props> = props => {
   return (
     <>
-    <style jsx global>{`
-      .container {
-        width: 920px;
-        margin: auto;
-      }
-      a {
-        word-wrap: break-word;
-      }
-      ul {
-        display: flex;
-        flex-wrap: wrap;
-        justify-content: space-evenly;
-        list-style: none;
-        padding: 0;
-        margin: 0;
-      }
-      li {
-        width: 150px;
-        padding: 8px;
-        border: 1px solid #ccc;
-        margin: 4px;
-        border-radius: 8px;
-        background-color: #f8f8f8;
-      }
-      .tags {
-        display: flex;
-        flex-wrap: wrap;
-      }
-      .tag {
-        background-color: #fff;
-        border: 1px solid #ccc;
-        padding: 2px;
-        border-radius: 4px;
-        margin: 2px;
-      }
-    `}</style>
-    <div className="container">
-      <h1>Qiita item</h1>
-      <ul>
-        {props.items.map(item =>
-          <li key={item.id}>
-            <div><Link href={`/qiita/[id]`} as={`/qiita/${item.id}`}><a>{item.title}</a></Link></div>
-            <div className="tags">{item.tags.map((tag, i) => <span key={i} className="tag">{tag.name}</span>)}</div>
-          </li>)}
-      </ul>
-    </div>
+      <style jsx global>{`
+        .container {
+          width: 920px;
+          margin: auto;
+        }
+        a {
+          word-wrap: break-word;
+        }
+        ul {
+          display: flex;
+          flex-wrap: wrap;
+          justify-content: space-evenly;
+          list-style: none;
+          padding: 0;
+          margin: 0;
+        }
+        li {
+          width: 150px;
+          padding: 8px;
+          border: 1px solid #ccc;
+          margin: 4px;
+          border-radius: 8px;
+          background-color: #f8f8f8;
+        }
+        .tags {
+          display: flex;
+          flex-wrap: wrap;
+        }
+        .tag {
+          background-color: #fff;
+          border: 1px solid #ccc;
+          padding: 2px;
+          border-radius: 4px;
+          margin: 2px;
+        }
+      `}</style>
+      <div className="container">
+        <h1>Qiita item</h1>
+        <ul>
+          {props.items.map(item => (
+            <li key={item.id}>
+              <div>
+                <Link href={`/qiita/[id]`} as={`/qiita/${item.id}`}>
+                  <a>{item.title}</a>
+                </Link>
+              </div>
+              <div className="tags">
+                {item.tags.map((tag, i) => (
+                  <span key={i} className="tag">
+                    {tag.name}
+                  </span>
+                ))}
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
     </>
   );
 };
@@ -86,12 +97,14 @@ class Cache {
 }
 
 async function fetchQiitaItems() {
-  return (await (await fetch("https://qiita.com/api/v2/items?per_page=100")).json());
+  return await (await fetch(
+    "https://qiita.com/api/v2/items?per_page=100"
+  )).json();
 }
 
 Qiita.getInitialProps = async () => {
   const json = await fetchQiitaItems();
   return { items: json };
-}
+};
 
 export default Qiita;
